b78a585a3bc0a07b62d4777e637a2b966b63005b
melohugo <hugoq2004@gmail.com>
2026-03-13 23:13:43 -0300

    refactor: dando update no flatpack e removendo packer do script

--- install.sh
+++ install.sh
@@ -13,6 +13,7 @@ sudo dnf -y update @core
 sudo dnf -y install gnome-tweak-tool dconf-editor
 
 flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
+flatpak update
 
 sudo dnf -y install curl
 
@@ -85,10 +86,6 @@ done
 
 # Theme of alacritty
 git clone https://github.com/alacritty/alacritty-theme ~/.config/alacritty/themes
-
-# Packer for neovim
-git clone --depth 1 https://github.com/wbthomason/packer.nvim\
- ~/.local/share/nvim/site/pack/packer/start/packer.nvim
 
 # TPM for tmux
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
